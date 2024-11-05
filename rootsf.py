c2620f25a2ecb2e7a2a83985f8e863e1b271b767
LeoUribC <leo.uribe1205@gmail.com>
2024-11-05 16:49:25 -0500

    generalized lagrange polynomial calculation

--- rootsf.py
+++ rootsf.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import sqrt, array
 
 
@@ -28,12 +29,68 @@ def muller_vec(x_points, f, epsi, tol=1e-10):
     return array([muller(x, f, epsi, tol) for x in x_points])
 
 
-def lagrange(x_points, y_points, x_eval):
-    # Generalizar despues...
-    l0 = ((x_eval - x_points[1]) / (x_points[0] - x_points[1])) * \
-        ((x_eval - x_points[2]) / (x_points[0] - x_points[2]))
-    l1 = ((x_eval - x_points[0]) / (x_points[1] - x_points[0])) * \
-        ((x_eval - x_points[2]) / (x_points[1] - x_points[2]))
-    l2 = ((x_eval - x_points[0]) / (x_points[2] - x_points[0])) * \
-        ((x_eval - x_points[1]) / (x_points[2] - x_points[1]))
-    return y_points[0, :] * l0 + y_points[1, :] * l1 + y_points[2, :] * l2
+def get_lagrange_basis(x_points, x_eval):
+    """
+    Computes the Lagrange basis polynomials evaluated at x_eval.
+
+    Args:
+    x_points: A numpy array of x-coordinates.
+    x_eval: The point at which to evaluate the basis polynomials.
+
+    Returns:
+    A numpy array of the evaluated basis polynomials.
+    """
+
+    # set up size of points and array to store basis polynomials
+    n = len(x_points)
+    basis_polynomials = np.zeros(n)
+
+    # main loop to get each basis polynomial
+    for i in range(n):
+        basis_polynomials[i] = 1.0
+        for j in range(n):
+            if j != i:
+                basis_polynomials[i] *= (x_eval - x_points[j]) / (x_points[i] - x_points[j])
+
+    return basis_polynomials
+
+
+def lagrange(x_points, y_points, x_eval) -> np.ndarray:
+    """
+    Computes a 1D array of Lagrange polynomials evaluated at given x_eval single points.
+
+    Args:
+    x_points (1D array): numpy 1D array-like of x values to compute each polynomial.
+    y_points (2D array): numpy 2D array-like, it stores rows of values for y for a vectorized computing.
+    x_eval (float): scalar value of x for evaluation of the Lagrange Polynomial.
+
+    Returns:
+    1D array of Lagrange Polynomials evaluated at a given x_eval point.
+    """
+
+    lagrange_basis = get_lagrange_basis(x_points, x_eval)
+    lagrange_poly = np.dot(y_points.T, lagrange_basis)
+
+    return lagrange_poly
+
+
+# previous implementation
+# def lagrange(x_points, y_points, x_eval):
+
+#     # Generalizar despues...
+#     l0 = ((x_eval - x_points[1]) / (x_points[0] - x_points[1])) * \
+#         ((x_eval - x_points[2]) / (x_points[0] - x_points[2]))
+#     l1 = ((x_eval - x_points[0]) / (x_points[1] - x_points[0])) * \
+#         ((x_eval - x_points[2]) / (x_points[1] - x_points[2]))
+#     l2 = ((x_eval - x_points[0]) / (x_points[2] - x_points[0])) * \
+#         ((x_eval - x_points[1]) / (x_points[2] - x_points[1]))
+
+#     result = y_points[0, :] * l0 + y_points[1, :] * l1 + y_points[2, :] * l2
+#     print(x_points.shape, y_points.shape, result.shape, x_eval)
+
+#     all_ls = np.array([l0, l1, l2])
+
+#     #return y_points[0, :] * l0 + y_points[1, :] * l1 + y_points[2, :] * l2
+#     return np.dot(y_points.T, all_ls)
+
+
