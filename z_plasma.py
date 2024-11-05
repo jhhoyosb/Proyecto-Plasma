27aa93a11449524117fb05fda8bc693c7fd54992
LeoUribC <leo.uribe1205@gmail.com>
2024-11-05 16:48:29 -0500

    using plasmapy plasma dispersion function

--- z_plasma.py
+++ z_plasma.py
@@ -1,9 +1,17 @@
-from scipy.special import erfc
-from numpy import sqrt, pi, exp
+#from scipy.special import erfc
+#from numpy import sqrt, pi, exp
+from plasmapy.dispersion import plasma_dispersion_func
 
 
-def z_plasma(z0):
+def z_plasma(z0) -> complex:
     """
-    z0: numpy array like of complex points to evaluate in zeta plasma function
+    Calculate the plasma dispersion function.
+
+    Parameters:
+    z0: numpy array like of complex points to evaluate in zeta plasma function.
+
+    Returns:
+    complex: The value of the plasma dispersion function.
     """
-    return 1j * sqrt(pi) * exp(-z0**2) * erfc(-1j * z0)
+    #return 1j * sqrt(pi) * exp(-z0**2) * erfc(-1j * z0)
+    return plasma_dispersion_func(z0)
