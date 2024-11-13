f5c5534a9e866a963f6796e67d4fca2e84e8beb3
LeoUribC <leo.uribe1205@gmail.com>
2024-11-13 11:02:47 -0500

    finish first implementation of run analysis method

--- core/prot_aniso.py
+++ core/prot_aniso.py
@@ -1,11 +1,15 @@
 
 from core.base_anisotropy import BaseAnisotropy
-from dispeprot import *  # temporary import syntax
+from dispeprot import dispeprot  # temporary import syntax
 
 
 
 class ProtAniso(BaseAnisotropy):
-
+    
+    """
+    Must keep in mind that the args for the dispeprot function are:
+        beta, A
+    """
 
     def __init__(self, beta, A, **kwargs):
 
@@ -18,10 +22,20 @@ class ProtAniso(BaseAnisotropy):
 
     def run_analysis(self):
 
-        zd = dispeprot(self.Xr_ps + self.Xi_ps * 1j,
-                       self.beta, self.yaxis[-1], self.A)
+        # creating the mesh
+        Xr_ps, Xi_ps = self.create_mesh()
+
+        zd = dispeprot( Xr_ps + Xi_ps * 1j,
+                        beta=self.beta, y=self.yaxis[-1], A=self.A )
         zdr = zd.real
         zdi = zd.imag
-        
-        self.plot_results(zdr, zdi)
+
+        # calling common plot method and get roots guest from user input
+        roots_guest = self.plot_results(Xr_ps, Xi_ps, zdr, zdi)
+
+        # perform root finding using common method
+        roots_wr, roots_wi = self._find_roots(
+            roots_guest, dispeprot, beta=self.beta, A=self.A )
+
+        self.plot_roots_analysis(self.yaxis, roots_wr, roots_wi, 0.3)
 
