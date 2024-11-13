ef442ca49a05a3aad6ddb50ca20307ce45b97c7c
LeoUribC <leo.uribe1205@gmail.com>
2024-11-13 16:38:28 -0500

    implemented run-protaniso-multi command

--- main.py
+++ main.py
@@ -44,8 +44,45 @@ def run_protaniso( beta: float = 0.2,
 
 
 @app.command()
-def say_hi(name:str):
-    print(f'hello {name}')
+def run_protaniso_multi( beta_a: float = 0.2,
+                         beta_p: float = 0.2,
+                         A_a: float = 4.0,
+                         A_p: float = 4.0,
+                         n_p: float = 4.0,
+                         gamma_max: float = 0.5,
+                         xr_max: float = 10,
+                         xr_min: float = -20,
+                         xi_max: float = 1.0,
+                         xi_min: float = -10.0,
+                         y_max: float = 4.0,
+                         np: int = 400,
+                         nir: int = 2000,
+                         nir_interp: int = 3,
+                         interp_grade: int = 2,
+                         epsi: float = 1.57e-7,
+                         eps_muller: float = 1e-7 ):
+
+    protaniso_multi_simulator = ProtAnisoMulti(
+        beta_a = beta_a,
+        beta_p = beta_p,
+        A_a = A_a,
+        A_p = A_p,
+        n_p = n_p,
+        gamma_max = gamma_max,
+        xr_max = xr_max,
+        xr_min = xr_min,
+        xi_max = xi_max,
+        xi_min = xi_min,
+        y_max = y_max,
+        np = np,
+        nir = nir,
+        nir_interp = nir_interp,
+        interp_grade = interp_grade,
+        epsi = epsi,
+        eps_muller = eps_muller
+    )
+
+    protaniso_multi_simulator.run_analysis()
 
 
 if __name__ == '__main__':
